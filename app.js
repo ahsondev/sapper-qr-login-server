5da63831db7c05521c43bd1a5b8758568c34e45e
unknown <you@example.com>
2021-08-22 15:29:50 -0300

    socket

--- app.js
+++ app.js
@@ -1,25 +1,19 @@
+const http = require('http')
+const express = require('express')
 
-const server = require('http').createServer();
-const io = require('socket.io')(server);
-const express = require('express');
+var cors = require('cors')
+var app = express()
+app.use(cors())
 
-var cors = require('cors');
-
-var app = express();
-app.use(cors());
-app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
+const server = http.createServer(app)
+const io = require("socket.io")(server, {
+  cors: {
+    origin: '*',
+  }
 });
 
-io.on('connection', client => {
-  client.on('event', data => { 
-    console.log("HEllo");
-    return data; 
-  });
-  client.on('disconnect', () => { /* … */ });
-});
-server.listen(3003);
+io.on('connection', () => {
+  /* … */
+})
+
+server.listen(3003)
